a32c0c9c3d38e6849f9d51ff88b970be16ccab2a
Bartlomiej Zaczek <bartlomiej.zaczek.contact@gmail.com>
2022-07-17 14:33:44 +0200

    Rename Json fields

--- src/main/java/org/githubproject/service/dto/Response.java
+++ src/main/java/org/githubproject/service/dto/Response.java
@@ -1,13 +1,22 @@
 package org.githubproject.service.dto;
 
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 @Data
 public class Response {
-
+    @JsonAlias("full_name")
+    @JsonProperty(value = "fullName", index= 0)
     private String full_name;
     private String description;
+    @JsonAlias("clone_url")
+    @JsonProperty(value = "cloneUrl")
     private String clone_url;
+    @JsonAlias("stargazers_count")
+    @JsonProperty(value = "stars")
     private String stargazers_count;
+    @JsonAlias("created_at")
+    @JsonProperty(value = "createdAt")
     private String created_at;
 }
